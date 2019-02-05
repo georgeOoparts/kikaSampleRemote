f5fd89ab4cdc77b41e1a812dfd3d8dfa0ff25b04
georgeOoparts <urataffy@gmail.com>
2019-02-06 00:21:49 +0900

    u4,5 シンプルに　ワールドとスクリーンの座標を表示

---
+++ u0004_1_LtScreenPositionSlide.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class u0004_1_LtScreenPositionSlide : MonoBehaviour {
+    //k4_1:どこかに書いてあるRectTransformの変数を作る
+    RectTransform rt;
+
+    void Start() {
+        //k4_1_1:このオブジェクトにＵＩ専門であるRectTransformをアタッチ
+        rt = this.gameObject.GetComponent<RectTransform>();
+    }
+
+    void Update() {
+        //k4_1_1_4:uiをスクリーン値で移動（左上にアンカーセット、下方向は-の値)
+        rt.anchoredPosition = new Vector2(0, 0);
+    }
+}
---
+++ u0005_1_ Lt2WorldPositionSlide.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class u0005_1_Lt2WorldPositionSlide : MonoBehaviour {
+    //まだ新公理にあわせず
+    //左上、上から2番目のテキストに貼り付けるプログラム
+    
+    //k4_a:どこかに書いてあるRectTransformの変数を作る
+    RectTransform rt;
+    //leftTopのテキストをインスペで当てはめる
+    public GameObject ltText;
+    //k2_a:どこかに書かれている。Textというクラスを扱うための変数を作成
+    Text ltTx;
+    // Use this for initialization
+    void Start() {
+        //k4_aa:このオブジェクトにＵＩ専門であるRectTransformをアタッチ
+        rt = this.gameObject.GetComponent<RectTransform>();
+        //k2_aa:Textをこのオブジェクトで使うためのおまじない
+        ltTx = ltText.GetComponent<Text>();
+    }
+    // Update is called once per frame
+    void Update() {
+        //一番左上のltTextの高さだけ下にポジショニング
+
+        //k2_aab2:スクリーン座標のテキスト高さ text.preferredHeight 
+        //k4_aac1:uiをスクリーン値で移動（左上にアンカーセット、下方向は-の値)
+        rt.anchoredPosition = new Vector2(0, -ltTx.preferredHeight);
+    }
+}

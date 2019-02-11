3a0000f9ee133c686e72e8b39ff614e4b3794c21
georgeOoparts <urataffy@gmail.com>
2019-02-12 01:38:11 +0900

    H7　まずは使いまわし変数p＿1＿1がつかえる事を確認

--- H_99_01_kyoutuHensu.cs
+++ H_99_01_kyoutuHensu.cs
@@ -7,9 +7,11 @@ public class H_99_01_kyoutuHensu : MonoBehaviour
     //共通で使い呼び出されるオブジェ、変数はここでかんりするぜ。
     //k5_3_1: gameobject(メソッド、変数)を外部から呼び出す。
     //ヒエでアタッチ
-    
+
     //マズ　ライン１の目次のオブジェを定める
-　　public GameObject p1_1;
+    public GameObject mokuji;
+
+    public GameObject p1_1;
     public GameObject p1_2;
     public GameObject p1_3;
     public GameObject p1_4;
---
+++ H_99_07_mokujiObjMove.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class H_99_07_mokujiObjMove : MonoBehaviour
+{
+    //k5_3_1_1:gameobject(メソッド、変数)を使いまわす
+    //このスクリプトをアタッチしたオブジェクトにいちいちこのオブジェクトをアタッチ
+    public H_99_01_kyoutuHensu kyotu;
+    //k5_3_1_1_1:gameobject(メソッド、変数)を使いまわす
+    //yosu.cube1で普通に使える
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(kyotu.p1_1.name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

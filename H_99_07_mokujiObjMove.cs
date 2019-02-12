8b98b70f11257b901a5c7924f6a7e393ba1081d3
georgeOoparts <urataffy@gmail.com>
2019-02-12 12:56:58 +0900

    H7　目次のパネルの位置を動かすプログラム。まずスタートでパネルを一列に並べ、一列に並んだパネルたちをアップデートで一気に動かす。

--- H_99_07_mokujiObjMove.cs
+++ H_99_07_mokujiObjMove.cs
@@ -7,17 +7,36 @@ public class H_99_07_mokujiObjMove : MonoBehaviour
     //k5_3_1_1:gameobject(メソッド、変数)を使いまわす
     //このスクリプトをアタッチしたオブジェクトにいちいちこのオブジェクトをアタッチ
     public H_99_01_kyoutuHensu kyotu;
-    //k5_3_1_1_1:gameobject(メソッド、変数)を使いまわす
-    //yosu.cube1で普通に使える
-    // Start is called before the first frame update
+
+    //k0013_1: 宣言
+    Transform trMokuji;
+
+    Transform trP1;
+    Transform trP2;
+    Transform trP3;
+    Transform trP4;
+    Transform trP5;
+    Transform trP6;
+
     void Start()
     {
-        Debug.Log(kyotu.p1_1.name);
-    }
+        //k0013_1_1;オブジェに当てはめる；
+        //k5_3_1_1_1:gameobject(メソッド、変数)を使いまわす
+        trMokuji = kyotu.mokuji.GetComponent<Transform>();
 
-    // Update is called once per frame
+        trP1 = kyotu.p1_1.GetComponent<Transform>();
+        trP2 = kyotu.p1_2.GetComponent<Transform>();
+        trP3 = kyotu.p1_3.GetComponent<Transform>();
+        trP4 = kyotu.p1_4.GetComponent<Transform>();
+        trP5 = kyotu.p1_5.GetComponent<Transform>();
+        trP6 = kyotu.p1_6.GetComponent<Transform>();
+
+        //k0013_1_1_1 オブジェ移動；オブジェの座標;z軸そのまま：オブジェのポジションを得る
+        trP1.position = new Vector3(trP1.position.x, 0, trP1.position.z);
+    }
     void Update()
     {
-        
+        trMokuji.position = new Vector3(trMokuji.position.x, trMokuji.position.y, 
+                                        trMokuji.position.z);
     }
 }

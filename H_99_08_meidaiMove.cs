57b44b033ce8ad759804aa9ca4097c55a626ed07
georgeOoparts <urataffy@gmail.com>
2019-02-14 11:00:04 +0900

    H8　命題MOVE作る。共通変数使いまわし成功。こっからｐ１＿１＿１～ｐ１＿１＿６の動きのプラグラムを書く

---
+++ H_99_08_meidaiMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class H_99_08_meidaiMove : MonoBehaviour
+{
+    //k5_3_1_1:gameobject(メソッド、変数)を使いまわす
+    //このスクリプトをアタッチしたオブジェクトにいちいちこのオブジェクトをアタッチ
+    public H_99_01_kyoutuHensu kyotu;
+
+    void Start()
+    {
+        Debug.Log("move?"+kyotu.p1_1_1.name);
+    }
+
+    void Update()
+    {
+        
+    }
+}

ba0bb2e403c516de8a82431a7d29981995452e69
georgeOoparts <urataffy@gmail.com>
2019-03-04 00:17:18 +0900

    単純タップ移動OK＞共通変数の切り替え

--- H_99_10_camTuchMove.cs
+++ H_99_10_camTuchMove.cs
@@ -11,10 +11,21 @@ public class H_99_10_camTuchMove : MonoBehaviour
     {
         //Debug.Log("koltukaaaaaa");
     }
-    //タッチした画面のオブジェの名前を得る、その時のオブジェを調べる奥行きの距離
-    float distance = 10.0f;
+   
     void Update()
     {
+        if (tuchObjName()=="panel1_1") {
+            
+            this.gameObject.transform.position = new Vector3(5.6f,0,-10);
+            Debug.Log(tuchObjName());
+        }
+    }
+    //タッチしたオブジェの名前を返すメソッド--------------------------------------------------------------------------------------------------
+    //タッチした画面のオブジェの名前を得る、その時のオブジェを調べる奥行きの距離
+    float distance = 10.0f;
+    //タッチしたオブジェの名前を返すメソッド
+    string tuchObjName() {
+        string objectName = "nothing";
         if (Input.GetMouseButtonDown(0)) {
             // クリックしたスクリーン座標をrayに変換
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
@@ -23,30 +34,35 @@ public class H_99_10_camTuchMove : MonoBehaviour
             // オブジェクトにrayが当たった時
             if (Physics.Raycast(ray, out hit, distance)) {
                 // rayが当たったオブジェクトの名前を取得
-                string objectName = hit.collider.gameObject.name;
-                Debug.Log(objectName);
+                objectName = hit.collider.gameObject.name;
+                return (objectName);
             }
+            return (objectName);
         }
+        return (objectName);
     }
-    
+    //--------------------------------------------------------------------------------------------------
 
-    //クリックした所にＵＩがあるかどうかを判定するメソッド
-    //private bool clickUiCheck() {
-    //    //マウスが対象ＵＩの上にあったらtrue,
-    //    //なかったらfalseを返す   
-    //    PointerEventData pointer
-    //            = new PointerEventData(EventSystem.current);
-    //    pointer.position = Input.mousePosition;
-    //    List<RaycastResult> result = new List<RaycastResult>();
-    //    EventSystem.current.RaycastAll(pointer, result);
 
-    //    foreach (RaycastResult raycastResult in result) {
-    //        // ここに名前を取得する処理を書く
-    //        // 複数ある場合は全て取得されるため注意
-    //        if (raycastResult.gameObject.name == "ruler") {
-    //            return (true);
-    //        }
-    //    }
-    //    return (false);
-    //}
+
+
 }
+//クリックした所にＵＩがあるかどうかを判定するメソッド
+//private bool clickUiCheck() {
+//    //マウスが対象ＵＩの上にあったらtrue,
+//    //なかったらfalseを返す   
+//    PointerEventData pointer
+//            = new PointerEventData(EventSystem.current);
+//    pointer.position = Input.mousePosition;
+//    List<RaycastResult> result = new List<RaycastResult>();
+//    EventSystem.current.RaycastAll(pointer, result);
+
+//    foreach (RaycastResult raycastResult in result) {
+//        // ここに名前を取得する処理を書く
+//        // 複数ある場合は全て取得されるため注意
+//        if (raycastResult.gameObject.name == "ruler") {
+//            return (true);
+//        }
+//    }
+//    return (false);
+//}

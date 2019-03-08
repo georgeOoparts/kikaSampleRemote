2d58758a6b469ac361c982f886ec34b9994af6ab
georgeOoparts <urataffy@gmail.com>
2019-03-08 15:58:19 +0900

    横フリック時ボタン移動しないようにした。

--- H_99_10_camTuchMove.cs
+++ H_99_10_camTuchMove.cs
@@ -28,7 +28,7 @@ public class H_99_10_camTuchMove : MonoBehaviour
 
         upDownClickPosition();
         if (hanteiSorF() == true) {//なぜかif文内の&&使えない
-            if (touchDistanceX() == false) {
+            if (touchDistanceX() == true) {
                 if (tuchObjName() == "panel1_1") {
                     //k5_3_1_1_1:gameobject(メソッド、変数)を使いまわす
                     kyotu.mainCameraPosi = 2;
@@ -40,6 +40,34 @@ public class H_99_10_camTuchMove : MonoBehaviour
                     //k5_3_1_1_1:gameobject(メソッド、変数)を使いまわす
                     kyotu.mainCameraPosi = 2;
                     kyotu.meidaiHensu = 2;
+                    kyotu.bottomMove = true;
+                    this.gameObject.transform.position = new Vector3(5.6f, 0, -10);
+                    //Debug.Log(tuchObjName());
+                } else if (tuchObjName() == "panel1_3") {
+                    //k5_3_1_1_1:gameobject(メソッド、変数)を使いまわす
+                    kyotu.mainCameraPosi = 2;
+                    kyotu.meidaiHensu = 3;
+                    kyotu.bottomMove = true;
+                    this.gameObject.transform.position = new Vector3(5.6f, 0, -10);
+                    //Debug.Log(tuchObjName());
+                } else if (tuchObjName() == "panel1_4") {
+                    //k5_3_1_1_1:gameobject(メソッド、変数)を使いまわす
+                    kyotu.mainCameraPosi = 2;
+                    kyotu.meidaiHensu = 4;
+                    kyotu.bottomMove = true;
+                    this.gameObject.transform.position = new Vector3(5.6f, 0, -10);
+                    //Debug.Log(tuchObjName());
+                } else if (tuchObjName() == "panel1_5") {
+                    //k5_3_1_1_1:gameobject(メソッド、変数)を使いまわす
+                    kyotu.mainCameraPosi = 2;
+                    kyotu.meidaiHensu = 5;
+                    kyotu.bottomMove = true;
+                    this.gameObject.transform.position = new Vector3(5.6f, 0, -10);
+                    //Debug.Log(tuchObjName());
+                } else if (tuchObjName() == "panel1_6") {
+                    //k5_3_1_1_1:gameobject(メソッド、変数)を使いまわす
+                    kyotu.mainCameraPosi = 2;
+                    kyotu.meidaiHensu = 6;
                     kyotu.bottomMove = true;
                     this.gameObject.transform.position = new Vector3(5.6f, 0, -10);
                     //Debug.Log(tuchObjName());
@@ -55,7 +83,8 @@ public class H_99_10_camTuchMove : MonoBehaviour
         if (Input.GetMouseButtonUp(0)) {
             float distance = atoClick.x - saishoClick.x;
 
-            if (distance >= 1.5 || distance <= -1.5)
+            //if (distance <= 0.1 || distance >= -0.1)
+            if (distance ==0)
                 return (true);
             else return (false);
         } else return (false);
@@ -66,13 +95,11 @@ public class H_99_10_camTuchMove : MonoBehaviour
 
     void upDownClickPosition(){
         if (Input.GetMouseButtonDown(0)) {
-            saishoClick = Input.mousePosition;
-            //k0003_6:スクリーン座標＞ワールド座標
-            saishoClick = Camera.main.ScreenToWorldPoint(saishoClick);
+           //k0003_6:スクリーン座標＞ワールド座標
+            saishoClick = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         } else if (Input.GetMouseButtonUp(0)) {
-            atoClick = Input.mousePosition;
             //k0003_6:スクリーン座標＞ワールド座標
-            atoClick = Camera.main.ScreenToWorldPoint(atoClick);
+            atoClick = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         }
     }
     // hanteiSorF():　判定SorF S(スワイプ)ならfalse、F（フリック）ならtrueを返すメソッド-----------------------------
@@ -84,7 +111,7 @@ public class H_99_10_camTuchMove : MonoBehaviour
     private float elapse;
 
     //時間判定の何秒以内かを決める変数。hanteiSorF()で使うswipeなら0,flickなら１を返す
-    public float hanteiSorFjikan = 0.45f;
+    public float hanteiSorFjikan = 0.2f;
 
     //判定SorF S(スワイプ)ならfalse、F（フリック）ならtrueを返す。
     bool hanteiSorF() {

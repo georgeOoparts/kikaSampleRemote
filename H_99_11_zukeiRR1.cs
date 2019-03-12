78e1b2c02752a7572e5b9f63715438f3d243e003
georgeOoparts <urataffy@gmail.com>
2019-03-12 19:29:12 +0900

    とりあえず3角形を表示　成功＞共通変数turnCountをつくり、三角形の効果を変化させる。

--- H_99_11_zukeiRR1.cs
+++ H_99_11_zukeiRR1.cs
@@ -5,13 +5,20 @@ using UnityEngine;
 public class H_99_11_zukeiRR1 : MonoBehaviour
 {
     //k0014_1 :プレハブ（画面のobjでもOK）を使う objにはりつけ
-    public GameObject line1;
-    public GameObject line2;
-    public GameObject line3;
+    public GameObject line;
+    //public GameObject line2;
+    //public GameObject line3;
 
     // Start is called before the first frame update
     void Start()
     {
+        //k0014_1_1 :プレハブを使う
+        GameObject line1= Instantiate(line);
+        GameObject line2 = Instantiate(line);
+        GameObject line3 = Instantiate(line);
+        //Instantiate(line2);
+        //Instantiate(line3);
+
         //k0015_99_1 :線を引くline回り　オブジェにラインをくっつける
         LineRenderer LR1 = line1.GetComponent<LineRenderer>();
         LineRenderer LR2 = line2.GetComponent<LineRenderer>();
@@ -27,15 +34,12 @@ public class H_99_11_zukeiRR1 : MonoBehaviour
         LR1.SetPosition(1, new Vector3(13.6f, 0f, 0f));
 
         LR2.SetPosition(0, new Vector3(13.6f, 0f, 0f));
-        LR2.SetPosition(1, new Vector3(13.6f, 3f, 0f));
+        LR2.SetPosition(1, new Vector3(11.6f, 3f, 0f));
 
-        LR3.SetPosition(0, new Vector3(13.6f, 3f, 0f));
+        LR3.SetPosition(0, new Vector3(11.6f, 3f, 0f));
         LR3.SetPosition(1, new Vector3(9.6f, 0f, 0f));
 
-        //k0014_1_1 :プレハブを使う
-        Instantiate(line1);
-        Instantiate(line2);
-        Instantiate(line3);
+        
 
         
     }

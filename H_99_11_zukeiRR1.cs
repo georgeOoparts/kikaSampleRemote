d2d63153bb1155a00c6dee5ea6deebde11802472
georgeOoparts <urataffy@gmail.com>
2019-03-12 18:04:40 +0900

    とりあえずプレハブからプログラムでラインを表示させた＞三角形を表示させる

--- H_99_11_zukeiRR1.cs
+++ H_99_11_zukeiRR1.cs
@@ -4,10 +4,14 @@ using UnityEngine;
 
 public class H_99_11_zukeiRR1 : MonoBehaviour
 {
+    //k0014_1 :プレハブ（画面のobjでもOK）を使う objにはりつけ
+    public GameObject line1;
     // Start is called before the first frame update
     void Start()
     {
-        Debug.Log("RRRRRRRRRRRRRrr");
+        //k0014_1_1 :プレハブを使う
+        Instantiate(line1);
+        //Debug.Log("RRRRRRRRRRRRRrr");
     }
 
     // Update is called once per frame

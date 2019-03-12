e421f4de30c710ed0621c557fb87570b93d1a852
georgeOoparts <urataffy@gmail.com>
2019-03-12 14:09:18 +0900

    リスト化成功＞メソッド合理化

--- H_99_07_mokujiObjMove.cs
+++ H_99_07_mokujiObjMove.cs
@@ -20,12 +20,6 @@ public class H_99_07_mokujiObjMove : MonoBehaviour
     //private List<Transform> trMeidai1 = new List<Transform>();
     private List<Transform> trMokujiChild = new List<Transform>();
 
-    Transform trP1;
-    Transform trP2;
-    Transform trP3;
-    Transform trP4;
-    Transform trP5;
-    Transform trP6;
 
     //start でpanel1～６の始まるｙ軸の値。
     float startPanel = 0;
@@ -54,33 +48,39 @@ public class H_99_07_mokujiObjMove : MonoBehaviour
         trMokujiChild.Add(kyotu.p1_5.GetComponent<Transform>());
         trMokujiChild.Add(kyotu.p1_6.GetComponent<Transform>());
 
-        trP1 = kyotu.p1_1.GetComponent<Transform>();
-        trP2 = kyotu.p1_2.GetComponent<Transform>();
-        trP3 = kyotu.p1_3.GetComponent<Transform>();
-        trP4 = kyotu.p1_4.GetComponent<Transform>();
-        trP5 = kyotu.p1_5.GetComponent<Transform>();
-        trP6 = kyotu.p1_6.GetComponent<Transform>();
+        
 
         //k0013_1_1_1 オブジェ移動；オブジェの座標;z軸そのまま：オブジェのポジションを得る
         //k0013_1_1_2 オブジェのx,y,z幅　取得　；変化させる；
         //page.localScale = new Vector3((float)-2.8, -5, page.position.z);
         //まずstartでパネルを一列に並べる
-        trP1.position = new Vector3(trP1.position.x, startPanel, trP1.position.z);
+        trMokujiChild[0].position = 
+            new Vector3(trMokujiChild[0].position.x, startPanel, trMokujiChild[0].position.z);
 
-        startPanel -=trP1.localScale.y/2+ trP2.localScale.y / 2+ spacePanel;
-        trP2.position = new Vector3(trP1.position.x, startPanel, trP1.position.z);
+        startPanel -= 
+            trMokujiChild[0].localScale.y/2 + trMokujiChild[1].localScale.y / 2+ spacePanel;
+        trMokujiChild[1].position = 
+            new Vector3(trMokujiChild[0].position.x, startPanel, trMokujiChild[0].position.z);
 
-        startPanel -= trP2.localScale.y / 2 + trP3.localScale.y / 2 + spacePanel;
-        trP3.position = new Vector3(trP1.position.x, startPanel, trP1.position.z);
+        startPanel -= 
+            trMokujiChild[1].localScale.y / 2 + trMokujiChild[2].localScale.y / 2 + spacePanel;
+        trMokujiChild[2].position = 
+            new Vector3(trMokujiChild[0].position.x, startPanel, trMokujiChild[0].position.z);
 
-        startPanel -= trP3.localScale.y / 2 + trP4.localScale.y / 2 + spacePanel;
-        trP4.position = new Vector3(trP1.position.x, startPanel, trP1.position.z);
+        startPanel -= 
+            trMokujiChild[2].localScale.y / 2 + trMokujiChild[3].localScale.y / 2 + spacePanel;
+        trMokujiChild[3].position = 
+            new Vector3(trMokujiChild[0].position.x, startPanel, trMokujiChild[0].position.z);
 
-        startPanel -= trP4.localScale.y / 2 + trP5.localScale.y / 2 + spacePanel;
-        trP5.position = new Vector3(trP1.position.x, startPanel, trP1.position.z);
+        startPanel -= 
+            trMokujiChild[3].localScale.y / 2 + trMokujiChild[4].localScale.y / 2 + spacePanel;
+        trMokujiChild[4].position = 
+            new Vector3(trMokujiChild[0].position.x, startPanel, trMokujiChild[0].position.z);
 
-        startPanel -= trP5.localScale.y / 2 + trP6.localScale.y / 2 + spacePanel;
-        trP6.position = new Vector3(trP1.position.x, startPanel, trP1.position.z);
+        startPanel -= 
+            trMokujiChild[4].localScale.y / 2 + trMokujiChild[5].localScale.y / 2 + spacePanel;
+        trMokujiChild[5].position = 
+            new Vector3(trMokujiChild[0].position.x, startPanel, trMokujiChild[0].position.z);
 
         //k0013_1_1_1 オブジェ移動；オブジェの座標;z軸そのまま：オブジェのポジションを得る
         //updateで並んだパネルを一気に動かす

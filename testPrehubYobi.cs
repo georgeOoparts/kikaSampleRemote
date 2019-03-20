6e41b48938b04b3385a0d30121f0684455094804
georgeOoparts <urataffy@gmail.com>
2019-03-20 18:54:09 +0900

    とりあえずプレハブでtext　panel呼び出すだけ＞親オブジェクト、サイズなどを指定する

---
+++ testPrehubYobi.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testPrehubYobi : MonoBehaviour
+{
+    //k0014_1 :プレハブ（画面のobjでもOK）を使う objにはりつけ
+    public GameObject preTextPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Debug.Log("eeeeeeeeeeeeeee"); 
+        //k0014_1_1 :プレハブを使う
+        Instantiate(preTextPanel);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

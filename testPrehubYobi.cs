494b9204c744211c0cc5ebc2a034d59b900de90f
georgeOoparts <urataffy@gmail.com>
2019-03-21 12:37:16 +0900

    list化、名前変更　成功＞文字を一列に並べる

--- testPrehubYobi.cs
+++ testPrehubYobi.cs
@@ -20,31 +20,41 @@ public class testPrehubYobi : MonoBehaviour
     public string coment="korejadamekaaaaaaaaa";
 
     GameObject kodomeText;
-    // Start is called before the first frame update
+
+    //k0016_99_1_1：listの宣言
+    private List<GameObject> ptp = new List<GameObject>();
     void Start()
     {
         //k0014_2_1 :プレハブを使う
-        GameObject ptp1 = Instantiate(preTextPanel) as GameObject;
 
-        GameObject ptp2 = Instantiate(preTextPanel) as GameObject;
+        //k0016_99_1_1_1：list新しい値を入れる
+        ptp.Add(Instantiate(preTextPanel) as GameObject);
+        ptp.Add(Instantiate(preTextPanel) as GameObject);
+        ptp.Add(Instantiate(preTextPanel) as GameObject);
+        //GameObject ptp1 = Instantiate(preTextPanel) as GameObject;
 
-        GameObject ptp3 = Instantiate(preTextPanel) as GameObject;
+        //GameObject ptp2 = Instantiate(preTextPanel) as GameObject;
+
+        //GameObject ptp3 = Instantiate(preTextPanel) as GameObject;
         //k0014_2_1_1 :プレハブをキャンバスの子供にする()
-        ptp1.transform.SetParent(canvas.transform, false);
-        ptp2.transform.SetParent(canvas.transform, false);
-        ptp3.transform.SetParent(canvas.transform, false);
+        ptp[0].transform.SetParent(canvas.transform, false);
+        ptp[1].transform.SetParent(canvas.transform, false);
+        ptp[2].transform.SetParent(canvas.transform, false);
 
-        rtPre = ptp1.gameObject.GetComponent<RectTransform>();
+        rtPre = ptp[0].gameObject.GetComponent<RectTransform>();
 
         //k8_1:子(孫、ひ孫　無限にＯＫ)になっているＯＢＪを得る。
         //自分の直下の子供を得る。
-        kodomeText = ptp1.transform.GetChild(0).gameObject;
+        kodomeText = ptp[0].transform.GetChild(0).gameObject;
 
         KT =kodomeText.GetComponent<Text>();
 
         //this.gameObject.transform.parent=preTextPanel.transform;
         //messageUI.transform.SetParent(canvas.transform, false);
         Debug.Log("OKKK");
+        ptp[0].name = "ptp0";
+        ptp[1].name = "ptp1";
+        ptp[2].name = "ptp2";
 
     }
 

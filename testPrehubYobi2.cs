361c07341958206cc892d70998a8fc51992c2672
georgeOoparts <urataffy@gmail.com>
2019-03-23 17:43:50 +0900

    とりあえずアタッチ＞textpanelの子にしてprehubを呼び出す。

--- testPrehubYobi2.cs
+++ testPrehubYobi2.cs
@@ -7,7 +7,7 @@ public class testPrehubYobi2 : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("uaaawaaaaaa");   
     }
 
     // Update is called once per frame

b5c11cd486e3a8cd122dce96bf6c7ca33cb23ff3
georgeOoparts <urataffy@gmail.com>
2019-03-23 23:00:56 +0900

    プレハブ多数呼び出しOK。＞文字入力関数を作る。

--- testPrehubYobi2.cs
+++ testPrehubYobi2.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class testPrehubYobi2 : MonoBehaviour
 {
@@ -12,7 +13,13 @@ public class testPrehubYobi2 : MonoBehaviour
     //k0016_99_1_1：listの宣言
     List<GameObject> ptp= new List<GameObject>();
 
-    
+    //mojipanelの子であるtextオブジェに当てはめるオブジェ
+    //k0016_99_1_1：listの宣言
+    //List<GameObject> kodomoObj = new List<GameObject>();
+
+    List<Text> kodomoText = new List<Text>();
+    List<GameObject> kodomoObj = new List<GameObject>();
+
 
 
     void Start()
@@ -22,13 +29,18 @@ public class testPrehubYobi2 : MonoBehaviour
             //プレハブを使う
             //k0016_99_1_1_1：list新しい値を入れる
             ptp.Add(Instantiate(premoji) as GameObject);
-
+            //premojiの子供オブジェであるtextをlistにする。
+            kodomoObj.Add(ptp[i].transform.GetChild(0).gameObject);
+            ////premojiの子供オブジェであるtextのコンポートメントであるTextをlistにする。
+            kodomoText.Add(kodomoObj[i].GetComponent<Text>());
+            
             //k0014_2_1_1 :プレハブをtextPanelの子供にする()
             ptp[i].transform.SetParent(this.gameObject.transform, false);
 
             //k0014_2_1_1: オブジェの名前を変化させる
             ptp[i].name = "ptp"+i;
-            ptp[i].transform.GetChild(0).name = "text"+i;
+            kodomoObj[i].name = "text"+i;
+
         }
 
 

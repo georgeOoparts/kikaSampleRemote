a2bb638f38dd6854eaad0ada55c3826be2b9f1ea
georgeOoparts <urataffy@gmail.com>
2019-03-28 16:01:18 +0900

    linerederer　list化＞他もlist化

--- H_99_11_zukeiRR1.cs
+++ H_99_11_zukeiRR1.cs
@@ -6,13 +6,11 @@ public class H_99_11_zukeiRR1 : MonoBehaviour
 {
     //k0014_1 :プレハブ（画面のobjでもOK）を使う objにはりつけ
     public GameObject line;
-    //public GameObject line2;
-    //public GameObject line3;
 
     //k0015_99_1 :線を引くline回り　オブジェにラインをくっつける
-    LineRenderer LR1;
-    LineRenderer LR2;
-    LineRenderer LR3;
+    List<LineRenderer> LR =new List<LineRenderer>();
+
+    
 
     public float upDown = 1;
     //public float rightLeft = 0;
@@ -28,14 +26,14 @@ public class H_99_11_zukeiRR1 : MonoBehaviour
         //Instantiate(line3);
 
         //k0015_99_1 :線を引くline回り　オブジェにラインをくっつける
-        LR1 = line1.GetComponent<LineRenderer>();
-        LR2 = line2.GetComponent<LineRenderer>();
-        LR3 = line3.GetComponent<LineRenderer>();
-
+        LR.Add(line1.GetComponent<LineRenderer>());
+        LR.Add(line2.GetComponent<LineRenderer>());
+        LR.Add(line3.GetComponent<LineRenderer>());
+       
         //k0015_99_1_2 :頂点の数（この場合２つ）
-        LR1.positionCount = 2;
-        LR2.positionCount = 2;
-        LR3.positionCount = 2;
+        LR[0].positionCount = 2;
+        LR[1].positionCount = 2;
+        LR[2].positionCount = 2;
 
             }
 
@@ -43,14 +41,14 @@ public class H_99_11_zukeiRR1 : MonoBehaviour
     void Update()
     {
         //k0015_99_1_3 :頂点を設定（どこからどこまで線を伸ばすか）
-        LR1.SetPosition(0, new Vector3(9.2f, 0f+upDown, -3f));
-        LR1.SetPosition(1, new Vector3(13.2f, 0f + upDown, -3f));
+        LR[0].SetPosition(0, new Vector3(9.2f, 0f+upDown, -3f));
+        LR[0].SetPosition(1, new Vector3(13.2f, 0f + upDown, -3f));
 
-        LR2.SetPosition(0, new Vector3(13.2f, 0f + upDown, -3f));
-        LR2.SetPosition(1, new Vector3(11.2f, 3.46f + upDown, -3f));
+        LR[1].SetPosition(0, new Vector3(13.2f, 0f + upDown, -3f));
+        LR[1].SetPosition(1, new Vector3(11.2f, 3.46f + upDown, -3f));
 
-        LR3.SetPosition(0, new Vector3(11.2f, 3.46f + upDown, -3f));
-        LR3.SetPosition(1, new Vector3(9.2f, 0f + upDown, -3f));
+        LR[2].SetPosition(0, new Vector3(11.2f, 3.46f + upDown, -3f));
+        LR[2].SetPosition(1, new Vector3(9.2f, 0f + upDown, -3f));
 
     }
 }

9af8355d8039953233005412f7514a088596dc9f
georgeOoparts <urataffy@gmail.com>
2019-04-15 16:25:03 +0900

    0000Aを作成＞0002Aを作る

---
+++ F0000A.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class F0000A : MonoBehaviour {
+    //0000A
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+}

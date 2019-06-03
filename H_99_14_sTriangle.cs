13aedba17f667fa64257c3a07c084fd5edb95f2c
georgeOoparts <urataffy@gmail.com>
2019-06-03 11:50:33 +0900

    とりあえずtriangleを呼び出す成功＞Mcountにより呼び出す。

--- H_99_14_sTriangle.cs
+++ H_99_14_sTriangle.cs
@@ -4,13 +4,25 @@ using UnityEngine;
 
 public class H_99_14_sTriangle : MonoBehaviour
 {
+    //オブジェsampleTriangleにアタッチ、
+    //共通変数MCountによってオブジェsampleTriangleの位置を変更
+
+    //k5_3_1_1:gameobject(メソッド、変数)を使いまわす
+    public H_99_01_kyoutuHensu kyotu;
+
+    Transform triMove;
+
     void Start()
     {
-        Debug.Log("samTriangle");    
+        triMove = this.gameObject.GetComponent<Transform>();
+
+
+        //k5_3_1_1_1:gameobject(メソッド、変数)を使いまわす
+        Debug.Log("samTriangle"+kyotu.MCount);    
     }
 
     void Update()
     {
-        
+        triMove.position = new Vector2(9.17f, 1.11f);
     }
 }

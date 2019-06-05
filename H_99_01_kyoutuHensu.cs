bb109f70eb02780ec1a11d0050b0b273687450cc
georgeOoparts <urataffy@gmail.com>
2019-06-05 21:10:49 +0900

    途中＞ストップウォッチで使うelapseを共通変数にする＞シーン完成

--- H_99_01_kyoutuHensu.cs
+++ H_99_01_kyoutuHensu.cs
@@ -8,6 +8,9 @@ public class H_99_01_kyoutuHensu : MonoBehaviour
     //k5_3_1: gameobject(メソッド、変数)を外部から呼び出す。
     //ヒエでアタッチ
 
+    
+    public float elapse=0.0f;    
+        
     //今、対象としているmeidaiを0から対応させる
     //どのプログラムで数が増える？
 

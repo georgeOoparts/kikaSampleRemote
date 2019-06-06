bf3321f392052688f7d80db77413d986d7971ae8
georgeOoparts <urataffy@gmail.com>
2019-06-06 12:05:23 +0900

    成功＞使いまわし変数をプログラムに適応

--- H_99_01B_kyotuElapse.cs
+++ H_99_01B_kyotuElapse.cs
@@ -6,27 +6,67 @@ public class H_99_01B_kyotuElapse : MonoBehaviour
 {
     //ストップウォッチで使う、共通変数であるelapseを作るプログラム
     //強調表現　点滅のブール変数tenmetuOnOffを作るプログラム
+
+    //共通変数であるelapse,tenmetuOnOff使いまわす
+
+
     //アタッチ＞mainncamera
 
 
 
+    //点滅強調に使う。kyochoHantei()メソッド--------------------------------------------------------
+    //void kyochouPanel(int count) の中で使う。
     //k6_a:ストップウォッチ関数を使う時のおまじない。
     //updateでelapse = (float)stopwatch.Elapsed.TotalSeconds;忘れずに 
     private System.Diagnostics.Stopwatch stopwatch
         = new System.Diagnostics.Stopwatch();
-    //このelapseは点滅表現で使うプログラムの共通変数として利用される
+    //このelapseはH_99_12_zukeiPPRRのほうで共通変数として利用される
     //それによって、点滅のタイミングを完全に合わせる
-    //強調表現点滅につかう時間を扱う変数
     public float elapse = 0;
-    void Start() {
+    private bool tenmetuOnOff = true;
+    public float interval = 0.35f;
+
+    bool kyochoHantei() 
+    {
+        if (tenmetuOnOff == true) 
+        {
+            if (elapse >= interval) 
+            {
+                tenmetuOnOff = false;
+
+                //k6_ab:ストップウォッチの時間をリセット
+                stopwatch.Reset();
+                //k6_aa:ストップウォッチスタート
+                stopwatch.Start();
+                return (tenmetuOnOff);
+            }
+            return (true);
+        } 
+        else if (elapse >= interval) 
+        {
+            tenmetuOnOff = true;
+
+            //k6_ab:ストップウォッチの時間をリセット
+            stopwatch.Reset();
+            //k6_aa:ストップウォッチスタート
+            stopwatch.Start();
+            return (tenmetuOnOff);
+        }
+        return (false);
+    }
+    void Start() 
+    {
         //k6_aa:ストップウォッチスタート
         stopwatch.Start();
     }
 
     // Update is called once per frame
-    void Update() {
+    void Update() 
+    {
         //k6_ac:何秒たったかを変数elapseに入れる
         elapse = (float)stopwatch.Elapsed.TotalSeconds;
-        Debug.Log(elapse);
+
+        kyochoHantei();
+        //Debug.Log(elapse+"::"+tenmetuOnOff);
     }
 }

5dba65b730153a0b7a32e56858c99b22be361810
georgeOoparts <urataffy@gmail.com>
2019-07-11 09:55:09 +0900

    listreset を何かボタンを押したら出来るようにした。

--- H_99_13_mojiHonban.cs
+++ H_99_13_mojiHonban.cs
@@ -42,96 +42,12 @@ public class H_99_13_mojiHonban : MonoBehaviour
     //k0016_99_2_1：2次元listの宣言
     private List<List<string>> kariList = new List<List<string>>();
     
-    
-    
-
-    //{"命題I-1","","","","","","","","","","","99"},
-    //                {"与えられた直線", "を","一辺","とする","正三角形","を作図する事。","","","","","","0"},
-    //                {"与えられた直線", "を","一辺","とする","正三角形","を作図する事。","","","","","","2"},
-    //                {"与えられた直線", "を","一辺","とする","正三角形","を作図する事。","","","","","","4"},
-    //                {"与えられた直線", "を","一辺","とする","正三角形","を作図する事。","","","","","","99"},
-    
     //k0016_99_1_1：listの宣言
     private List<string> M1 = new List<string>();
 
     void Start() 
     {
-        //2次元配列にmojipanelに入れる文字をセットする。
-        //setWord();
-
         
-        //Debug.Log(intValues.GetLength(0)+"::"+ intValues.GetLength(1));
-
-
-        
-
-        //kariList.Add(meidaiSita);
-        //for (int i=0;i<meidaiSita.Count;i++) 
-        //{
-          //  kariList[0].Add(meidaiSita[i]);
-        //}
-
-
-        //Debug.Log("OKK::"+ kariList[0][1]);
-        //Debug.Log("OKK::");
-
-
-
-        ////4元目に3元目を当てはめる
-        ////>bunsho[0]を作成 定義　0
-        //bunsho.Add(new List<List<List<string>>>());
-        ////>bunsho[1]を作成　公準　1
-        //bunsho.Add(new List<List<List<string>>>());
-        ////>bunsho[2]を作成　公理　2
-        //bunsho.Add(new List<List<List<string>>>());
-        ////>bunsho[3]を作成　meidai　3
-        //bunsho.Add(new List<List<List<string>>>());
-
-        ////3元目に2元目を当てはめる
-        ////>bunsho[0][0]を作成
-        //bunsho[0].Add(new List<List<string>>());
-        ////>bunsho[0][1]を作成
-        //bunsho[0].Add(new List<List<string>>());
-
-        //3元目に2元目を当てはめる
-        //>bunsho[3][0]を作成 meidai 1～6までの6個のmeidaiに対応
-
-        //for(int i=0; i<6;i++) 
-        //{
-        //    bunsho[3].Add(new List<List<string>>());
-        //}
-        ///k0016_99_1_1_6　：List 直接値をいれて定義する。
-        //private List<float> meidaiSita = new List<float> { 8.05f, 15.2f,8.9f,4.9f,12.3f,10.0f };
-
-
-
-
-        ////2元目に1元目を当てはめる
-        ////>bunsho[0][0][0]を作成
-        //bunsho[0][0].Add(new List<string>());
-        ////>bunsho[0][0][1]を作成
-        //bunsho[0][0].Add(new List<string>());
-        ////>bunsho[0][0][2]を作成
-        //bunsho[0][0].Add(new List<string>());
-
-        //2元目に1元目を当てはめる　meidai１の文章数13
-        //>bunsho[3][0][0]～[3][0][12]を作成 meidai1の文章 1～13に対応
-        //for (int i = 0; i < 13; i++) 
-        //{
-        //    bunsho[3][0].Add(new List<string>());
-        //}
-        //bunsho[3][0]
-
-        //bunsho[3][0] ={ { 0,1},{ 0,1} };
-
-        //1元目にデータを当てはめる
-        //bunsho[0][0][0][0]を作成
-        //bunsho[0][0][0].Add("4jigen");
-
-
-        //Debug.Log("やりなおし::"+ bunsho[0][0][0][0]);
-        //配列に解説文を割り当てる。
-        //setWord();
         //ここから↓ないとバグ出る-----------------
         for (int i = 0; i < 11; i++) 
         {
@@ -152,56 +68,51 @@ public class H_99_13_mojiHonban : MonoBehaviour
         }
         //ここから↑ないとバグ出る-----------------
     }
-    ///k0016_99_1_1_6　：List 直接値をいれて定義する。
-    //List<string> meidaiSita1_1 = new List<string>
-    //{"命題I-1","","","","","","","","","","","99"};
+   
 
     private int kyouchouHenkanInt;
     void Update() 
     {
         if (Input.GetKeyDown("0")) 
         {
+            listReset();
             kyotu.mojiSwitch = 0;
         } 
         else if (Input.GetKeyDown("1")) 
         {
+            listReset();
             kyotu.mojiSwitch = 1;
         } 
         else if (Input.GetKeyDown("2")) 
         {
+            listReset();
             kyotu.mojiSwitch = 2;
         } 
         else if (Input.GetKeyDown("3")) 
         {
+            listReset();
             kyotu.mojiSwitch = 3;
         }
 
         //kyotu.rrCountの数を増やす
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) 
+        {
+            listReset();
             kyotu.rrCount++;
-
+        } 
         else if (Input.GetMouseButtonDown(1)) 
         {
+            listReset();
             kyotu.MCount++;
             kyotu.rrCount = 0;
         }
-        listReset();
-        hairetutolist();
-        //文字パネルに入れるlistに内容を入れる
-        //for (int i = 0; i < m1_0.GetLength(0); i++) 
-        //{
-        //    //k0016_99_2_1_1：2次元list [0][],[1][]をつくる
-        //    kariList.Add(new List<string>());
+        //listReset();
+        hairetuToList();
+        
 
-        //    for (int j = 0; j < m1_0.GetLength(1); j++) {
-        //        kariList[i].Add(m1_0[i, j]);
-        //    }
-        //}
-
-        //UItextに2次元配列の値を入れていく＞＞ここだけいちいち変える必要が有る
+        //UItextに2次元配列の値を入れていく
         for (int i = 0; i < kodomoTextText.Count; i++) 
         {
-
             if (kyotu.rrCount < m1_0.GetLength(0)) 
             {
                 //mojipanelの各UItextに文字を代入していく
@@ -212,7 +123,7 @@ public class H_99_13_mojiHonban : MonoBehaviour
                 //最終行を出力し続ける:mojipanelの各UItextに文字を代入していく
                 kodomoTextText[i].text = kariList[m1_0.GetLength(0) - 1][i];
             }
-        }//Debug.Log(intValues[intValues.GetLength(0)-1, intValues.GetLength(1)-1]);
+        }
 
         //文字が入った配列の情報から、強調すべき文字の順番をintで得る
         //k10_2:strin>int変換
@@ -235,7 +146,7 @@ public class H_99_13_mojiHonban : MonoBehaviour
     }
 
     //配列からlistへ文字を送る
-    void hairetutolist() 
+    void hairetuToList() 
     {
         //文字パネルに入れるlistに内容を入れる
         for (int i = 0; i < m1_0.GetLength(0); i++) {
@@ -260,14 +171,14 @@ public class H_99_13_mojiHonban : MonoBehaviour
 
     private string[,] m1_1 = new string[,]
     {
-       {"AB","を与えられた","直線","とする。","","","","","","","","0"},
-       {"AB","を与えられた","直線","とする。","","","","","","","","99"},
+        {"AB","を与えられた","直線","とする。","","","","","","","","0"},
+        {"AB","を与えられた","直線","とする。","","","","","","","","99"},
         {"このとき、", "AB","を一辺とする","正三角形","を作図することが","求められている。","","","","","","1"},
-                {"このとき、", "AB","を一辺とする","正三角形","を作図することが","求められている。","","","","","","3"},
-                {"このとき、", "AB","を一辺とする","正三角形","を作図することが","求められている。","","","","","","99"},
-                {"A", "を中心とし、","AB","を半径とする円","BCD","を描く[公準P-3]","","","","","","0"},
-                {"A", "を中心とし、","AB","を半径とする円","BCD","を描く[公準P-3]","","","","","","2"},
-                {"A", "を中心とし、","AB","を半径とする円","BCD","を描く[公準P-3]","","","","","","4"},
+        {"このとき、", "AB","を一辺とする","正三角形","を作図することが","求められている。","","","","","","3"},
+        {"このとき、", "AB","を一辺とする","正三角形","を作図することが","求められている。","","","","","","99"},
+        {"A", "を中心とし、","AB","を半径とする円","BCD","を描く[公準P-3]","","","","","","0"},
+        {"A", "を中心とし、","AB","を半径とする円","BCD","を描く[公準P-3]","","","","","","2"},
+        {"A", "を中心とし、","AB","を半径とする円","BCD","を描く[公準P-3]","","","","","","4"},
                 {"A", "を中心とし、","AB","を半径とする円","BCD","を描く[公準P-3]","","","","","","99"},
                 {"BA", "を半径とする円","ACE","を描く[公準P-3]。","","","","","","","","0"},
                 {"BA", "を半径とする円","ACE","を描く[公準P-3]。","","","","","","","","2"},

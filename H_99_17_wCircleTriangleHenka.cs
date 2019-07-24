a7e626b6b47d1e93bff4282b874c7d7eb5f6367b
georgeOoparts <urataffy@gmail.com>
2019-07-24 15:02:36 +0900

    前準備完了＞ｍ１＿１　RR

--- H_99_17_wCircleTriangleHenka.cs
+++ H_99_17_wCircleTriangleHenka.cs
@@ -47,13 +47,67 @@ public class H_99_17_wCircleTriangleHenka : MonoBehaviour
     public GameObject wcCylinder2;
 
     //いちいちunityで当てはめなきゃ駄目　↑---------------
-    //線分を出したり消したりするときに使う 
+    //線分を出したり消したりするときに使う Rendererを当てはめる
+    //線のrenderer
+    private Renderer rrlineAB;
+    private Renderer rrlineSAB;
 
+    private Renderer rrlineBC;
+    private Renderer rrlineSBC;
 
+    private Renderer rrlineCA;
+    private Renderer rrlineSCA;
+
+    //点のrenderer
+    private Renderer rrPointA;
+    private Renderer rrTextA;
+    private Renderer rrStrongTextCubeA;
+
+    private Renderer rrPointB;
+    private Renderer rrTextB;
+    private Renderer rrStrongTextCubeB;
+
+    private Renderer rrPointC;
+    private Renderer rrTextC;
+    private Renderer rrStrongTextCubeC;
+
+    //後ろの２つの円のrenderer
+    private Renderer rrOyaCylinder1;
+    private Renderer rrCylinder1;
+
+    private Renderer rrOyaCylinder2;
+    private Renderer rrCylinder2;
 
     void Start()
     {
-        
+        //redererを当てはめていく
+        rrlineAB = wcLineAB.GetComponent<Renderer>();
+        rrlineSAB = wcLineSAB.GetComponent<Renderer>();
+
+        rrlineBC = wcLineBC.GetComponent<Renderer>();
+        rrlineSBC = wcLineSBC.GetComponent<Renderer>();
+
+        rrlineCA = wcLineCA.GetComponent<Renderer>();
+        rrlineSCA = wcLineSCA.GetComponent<Renderer>();
+
+        rrPointA = wcPointA.GetComponent<Renderer>();
+        rrTextA = wcTextA.GetComponent<Renderer>();
+        rrStrongTextCubeA = wcStrongTextCubeA.GetComponent<Renderer>();
+
+        rrPointB = wcPointB.GetComponent<Renderer>();
+        rrTextB = wcTextB.GetComponent<Renderer>();
+        rrStrongTextCubeB = wcStrongTextCubeB.GetComponent<Renderer>();
+
+        rrPointC = wcPointC.GetComponent<Renderer>();
+        rrTextC = wcTextC.GetComponent<Renderer>();
+        rrStrongTextCubeC = wcStrongTextCubeC.GetComponent<Renderer>();
+
+        // 後ろの２つの円のrenderer
+        rrOyaCylinder1=wcOyaCylinder1.GetComponent<Renderer>();
+        rrCylinder1= wcCylinder1.GetComponent<Renderer>();
+
+        rrOyaCylinder2 = wcOyaCylinder2.GetComponent<Renderer>();
+        rrCylinder2 = wcCylinder2.GetComponent<Renderer>();
     }
 
     // Update is called once per frame

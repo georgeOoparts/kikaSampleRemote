9659e1ed00801d3a13c2f630d9d09c7406d7472a
georgeOoparts <urataffy@gmail.com>
2019-08-12 06:55:46 +0900

    バックボタン追加

---
+++ H_99_22_BackSwitch.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class H_99_22_BackSwitch : MonoBehaviour {
+    //M1_2Objectにアタッチ、countで変化させる
+
+    //いちいちunityで当てはめなきゃ駄目　↓---------------
+
+    //k5_3_1_1:gameobject(メソッド、変数)を使いまわす
+    public H_99_01_kyoutuHensu kyotu;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log("switch::" + kyotu.mojiSwitch + "::MC::" + kyotu.MCount + "::RRC::" + kyotu.rrCount);
+    }
+}

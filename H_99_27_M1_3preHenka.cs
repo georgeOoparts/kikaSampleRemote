3e3cbd130daa5e7dd9c84ca110c412b75d0eda76
georgeOoparts <urataffy@gmail.com>
2019-08-28 01:56:07 +0900

    m1_3rr 変数当てはめ

--- H_99_27_M1_3preHenka.cs
+++ H_99_27_M1_3preHenka.cs
@@ -13,9 +13,51 @@ public class H_99_27_M1_3preHenka : MonoBehaviour
 
     //強調表現点滅に使う変数を共通変数として使う。
     public H_99_01B_kyotuElapse kyotuEla;
+
+    //lineを当てはめる
+    public GameObject M1_3preLineS;
+    public GameObject M1_3preLineStS;
+
+    public GameObject M1_3preLineL;
+    public GameObject M1_3preLineStL;
+
+    public GameObject M1_3preLineS2;
+    public GameObject M1_3preLineStS2;
+
+    public GameObject M1_3preLineL_S;
+    public GameObject M1_3preLineStL_S;
+
+    //renderer当てはめ
+    //＞linerederer当てはめ
+    private Renderer rrM1_3preLineS;
+    private Renderer rrM1_3preLineStS;
+
+    private Renderer rrM1_3preLineL;
+    private Renderer rrM1_3preLineStL;
+
+    private Renderer rrM1_3preLineS2;
+    private Renderer rrM1_3preLineStS2;
+
+    public Renderer rrM1_3preLineL_S;
+    public Renderer rrM1_3preLineStL_S;
+
     void Start()
     {
-        
+        //gameobjectをredererに当てはめていく
+        //＞line：gameobjectをredererに当てはめていく
+        rrM1_3preLineS = M1_3preLineS.GetComponent<Renderer>();
+        rrM1_3preLineStS = M1_3preLineStS.GetComponent<Renderer>();
+
+        rrM1_3preLineL = M1_3preLineL.GetComponent<Renderer>();
+        rrM1_3preLineStL = M1_3preLineStL.GetComponent<Renderer>();
+
+        rrM1_3preLineS2 = M1_3preLineS2.GetComponent<Renderer>();
+        rrM1_3preLineStS2 = M1_3preLineStS2.GetComponent<Renderer>();
+
+        rrM1_3preLineL_S = M1_3preLineL_S.GetComponent<Renderer>();
+        rrM1_3preLineStL_S = M1_3preLineStL_S.GetComponent<Renderer>();
+
+
     }
 
     // Update is called once per frame

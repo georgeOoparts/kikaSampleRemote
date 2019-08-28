ea09388a5a27722985f4c9092f09eab0698c8d7e
georgeOoparts <urataffy@gmail.com>
2019-08-28 09:29:50 +0900

    m1_3rr　完成

--- H_99_28_M1_3RR.cs
+++ H_99_28_M1_3RR.cs
@@ -13,12 +13,18 @@ public class H_99_28_M1_3RR : MonoBehaviour
     private Transform M1_3RRMove;
     void Start()
     {
-        
+        M1_3RRMove = this.gameObject.GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //meidai  m1_3 count4以下
+        if (kyotu.mojiSwitch == 3 && kyotu.MCount == 2 && kyotu.rrCount >= 5) {
+            M1_3RRMove.position = new Vector3(9.8f, 3.02f, -1);
+        } else {
+            M1_3RRMove.position = new Vector3(16.35f, -3.74f, 0);
+        }
         Debug.Log("M1_3RR::" + kyotu.mojiSwitch + "::MC::" + kyotu.MCount + "::RRC::" + kyotu.rrCount);
     }
 }

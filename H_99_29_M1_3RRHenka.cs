c0cdcb4bc4360633a8dfbaf2cec26b1264c6e91d
georgeOoparts <urataffy@gmail.com>
2019-08-28 11:33:58 +0900

    m1_3rr　reset関数、count関数入れるの完了

--- H_99_29_M1_3RRHenka.cs
+++ H_99_29_M1_3RRHenka.cs
@@ -2,8 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class H_99_29_M1_3RRHenka : MonoBehaviour
-{
+public class H_99_29_M1_3RRHenka : MonoBehaviour {
     //M1_3preにアタッチ。オブジェクト全体のいどうをするプログラム
     //いちいちunityで当てはめなきゃ駄目　↓---------------
 
@@ -68,7 +67,7 @@ public class H_99_29_M1_3RRHenka : MonoBehaviour
 
     private Renderer rrM1_3LineC;
     private Renderer rrM1_3LineSC;
- 
+
     //＞＞point rederer当てはめ
     private Renderer rrM1_3PointA;
     private Renderer rrM1_3PointB;
@@ -93,12 +92,11 @@ public class H_99_29_M1_3RRHenka : MonoBehaviour
     private Renderer rrM1_3TextD;
     private Renderer rrM1_3TextE;
     private Renderer rrM1_3TextF;
-   
-    void Start()
-    {
-       //gameobjectをredererに当てはめていく
+
+    void Start() {
+        //gameobjectをredererに当てはめていく
         //＞line：gameobjectをredererに当てはめていく
-        rrM1_3LineAE =M1_3LineAE.GetComponent<Renderer>();
+        rrM1_3LineAE = M1_3LineAE.GetComponent<Renderer>();
         rrM1_3LineSAE = M1_3LineSAE.GetComponent<Renderer>();
 
         rrM1_3LineAB = M1_3LineAB.GetComponent<Renderer>();
@@ -139,9 +137,76 @@ public class H_99_29_M1_3RRHenka : MonoBehaviour
         rrM1_3TextF = M1_3TextF.GetComponent<Renderer>();
     }
     // Update is called once per frame
-    void Update()
-    {
+    void Update() {
+        henka(kyotu.rrCount);
         Debug.Log("M1_3RRHenka::" + kyotu.mojiSwitch + "::MC::" + kyotu.MCount + "::RRC::" + kyotu.rrCount);
 
+    }
+    //リセット表示を全部消すメソッド　kyotu.rrcount-----------------------------
+    private void reset() {
+        //k7B_1_1:オブジェを存在するけど見えなくする。
+        //this.gameObject.GetComponent<Renderer>().enabled = false; 
+
+        //k7B_1_2:オブジェを見えるようにするよ。
+        //this.gameObject.GetComponent<Renderer>().enabled = true;
+
+        //gameobjectを見えなくする
+        //＞line：gameobjectを見えなくする
+
+        rrM1_3LineAE.enabled = false;
+        rrM1_3LineSAE.enabled = false;
+
+        rrM1_3LineAB.enabled = false;
+        rrM1_3LineSAB.enabled = false;
+
+        rrM1_3LineAD.enabled = false;
+        rrM1_3LineSAD.enabled = false;
+
+        rrM1_3LineEB.enabled = false;
+        rrM1_3LineSEB.enabled = false;
+
+        rrM1_3LineC.enabled = false;
+        rrM1_3LineSC.enabled = false;
+
+        //＞point：gameobjectを見えなくする
+        rrM1_3PointA.enabled = false;
+        rrM1_3PointB.enabled = false;
+        rrM1_3PointD.enabled = false;
+        rrM1_3PointE.enabled = false;
+        rrM1_3PointF.enabled = false;
+        //＞pointSt：gameobjectを見えなくする
+        rrM1_3StPointA.enabled = false;
+        rrM1_3StPointB.enabled = false;
+        rrM1_3StPointD.enabled = false;
+        rrM1_3StPointE.enabled = false;
+        rrM1_3StPointF.enabled = false;
+
+        ////＞circle：gameobjectを見えなくする
+        rrM1_3Circle.enabled = false;
+        rrM1_3StCircle.enabled = false;
+
+        ////＞text：gameobjectを見えなくする
+        rrM1_3TextA.enabled = false;
+        rrM1_3TextB.enabled = false;
+        rrM1_3TextC.enabled = false;
+        rrM1_3TextD.enabled = false;
+        rrM1_3TextE.enabled = false;
+        rrM1_3TextF.enabled = false;
+    }
+    //rrCountでオブジェの表示、強調を変化させるメソッド---------------------------
+    private void henka(int count) {
+        //m1_3のときのみ実行
+        if (kyotu.mojiSwitch == 3 & kyotu.MCount == 2) {
+            if (count == 5) {
+                reset();
+                //gameobjectを見えるようにする
+                //＞point：gameobjectを見えるようにする
+                //rrPointA.enabled = true;
+                //rrStPointA.enabled = kyotuEla.tenmetuOnOff;
+
+                //////＞text：gameobjectを見えるようにする
+                //rrTextA.enabled = true;
+            }
+        }
     }
 }

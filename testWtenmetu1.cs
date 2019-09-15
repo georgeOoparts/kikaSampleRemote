c6d2d34bc7dccca7280177c8103c823f619cb340
georgeOoparts <urataffy@gmail.com>
2019-09-16 06:20:05 +0900

    testWtenmetu1 2交互強調テスト　成功

---
+++ testWtenmetu1.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testWtenmetu1 : MonoBehaviour
+{
+    //強調表現点滅に使う変数を共通変数として使う。
+    public H_99_01B_kyotuElapse kyotuEla;
+
+    public GameObject LineAB;
+
+    private Renderer rrLineAB;
+
+    void Start()
+    {
+        rrLineAB = LineAB.GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rrLineAB.enabled = kyotuEla.tenmetuOnOff;
+        //Debug.Log("ABBBBBB");
+    }
+}
---
+++ testWtenmetu2.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testWtenmetu2 : MonoBehaviour
+{
+    //強調表現点滅に使う変数を共通変数として使う。
+    public H_99_01B_kyotuElapse kyotuEla;
+
+    public GameObject LineAC;
+
+    private Renderer rrLineAC;
+    void Start()
+    {
+        rrLineAC = LineAC.GetComponent<Renderer>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rrLineAC.enabled = !(kyotuEla.tenmetuOnOff);
+
+        //Debug.Log("CDDDDDD");
+    }
+}

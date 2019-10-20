3c12de0689c5afe36a14815389c84f70976cc318
georgeOoparts <urataffy@gmail.com>
2019-10-21 06:13:33 +0900

    kjprr完成

--- H_99_40_KJP_3RR.cs
+++ H_99_40_KJP_3RR.cs
@@ -9,15 +9,23 @@ public class H_99_40_KJP_3RR : MonoBehaviour
 
     //k5_3_1_1:gameobject(メソッド、変数)を使いまわす
     public H_99_01_kyoutuHensu kyotu;
-    void Start()
+
+    private Transform KJP_3Move;
+
+    void Start() 
     {
-        
+        KJP_3Move = this.gameObject.GetComponent<Transform>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update() 
     {
-        Debug.Log("KJP_3RR::" + kyotu.mojiSwitch + "::MC::" + kyotu.MCount + "::RRC::" + kyotu.rrCount);
+        //meidai  m1_5 count4以下
+        if (kyotu.mojiSwitch == 1 && kyotu.MCount == 2) {
+            KJP_3Move.position = new Vector3(11.18f, 2.6f, -1);
+        } else {
+            KJP_3Move.position = new Vector3(20.35f, -3.74f, 0);
+        }
+        //Debug.Log("KJP_3RR::" + kyotu.mojiSwitch + "::MC::" + kyotu.MCount + "::RRC::" + kyotu.rrCount);
 
     }
 }
--- H_99_41_KJP_3RRHenka.cs
+++ H_99_41_KJP_3RRHenka.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class H_99_41_KJP_3RRHenka : MonoBehaviour
 {
-    // Start is called before the first frame update
+    //M1_5RRにアタッチ。オブジェクト全体のいどうをするプログラム
+    //いちいちunityで当てはめなきゃ駄目　↓---------------
+
+    //k5_3_1_1:gameobject(メソッド、変数)を使いまわす
+    public H_99_01_kyoutuHensu kyotu;
+
+    //強調表現点滅に使う変数を共通変数として使う。
+    public H_99_01B_kyotuElapse kyotuEla;
     void Start()
     {
         
@@ -13,6 +20,7 @@ public class H_99_41_KJP_3RRHenka : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Debug.Log("KJP_3RRHenka::" + kyotu.mojiSwitch + "::MC::" + kyotu.MCount + "::RRC::" + kyotu.rrCount);
+
     }
 }
